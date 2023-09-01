5379688ed5b37408fc20739480dc3e103417289e
abiabi31 <abishsabi31@gmail.com>
2023-08-31 20:33:53 -0700

    abish

---
+++ src/abish/Tree.java
@@ -0,0 +1,11 @@
+package abish;
+
+public class Tree {
+	 public void abi( String name,int age){
+	 System.out.println("his name is:"+name+"\n"+"age is:"+age);
+	  }
+	  public static void main(String[]args){
+	  Tree shek=new Tree();
+	  shek.abi("abi", 43);
+	}
+	}
