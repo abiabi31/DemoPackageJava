535027d016c19c7950465797af3feaea422ca607
abiabi31 <abishsabi31@gmail.com>
2023-09-16 20:55:17 +0530

    abish

--- src/abii/Fire.java
+++ src/abii/Fire.java
@@ -37,9 +37,7 @@ public class Fire {
 	}
 	else {
 		System.out.println("LAMBORGHINI");
-		
-		
+			
 	}
-	
 	}
 }
---
+++ src/abii/Fire9.java
@@ -0,0 +1,48 @@
+package abii;
+
+import java.util.Scanner;
+public class Fire9 {
+
+	public void air10() {
+	int mark;
+	System.out.println("Mark ender");
+     Scanner sun=new Scanner(System.in);
+     mark =sun.nextInt();
+     
+     switch (mark) {
+     case 10:
+    	 System.out.println("very very bad");
+     case 20:
+    	 System.out.println("very bad");
+     break;    
+     case 30:
+    	 System.out.println("fail");
+     break;    
+     case 40:
+    	 System.out.println("just pass");
+     break;    
+     case 50:
+    	 System.out.println("pass");
+     break;    
+     case 60:
+    	 System.out.println("just okey");
+     break;    
+     case 70:
+    	 System.out.println("good");
+     break;    
+     case 80:
+    	 System.out.println("ver good");
+     break;    
+     case 90:
+    	 System.out.println("very very good");
+     break;    
+     default:
+    	 System.out.println("excellent");
+     break;     
+     
+     }
+     }public static void main(String[] args) {
+		Fire9 fire9=new Fire9();
+		fire9.air10();
+	}
+}
