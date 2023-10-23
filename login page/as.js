ea1e681770c4c97348025770d643e1387e86fed0
abiabi31 <abishsabi31@gmail.com>
2023-10-23 12:11:20 +0530

    javascript

---
+++ login page/as.js
@@ -0,0 +1,27 @@
+
+// alert("the file is open"); 
+// function abis(){
+//     document.getElementById('ggt').innerHTML="lamborghinie"
+    
+// }
+// function a(){
+// //  document.getElementById('ggt').innerHTML=5+6
+// //  console.log(5+6)
+// // print()
+// document.getElementById('demo1').innerHTML="black"
+// }
+// function as(){
+
+//  let a, b, c;
+// a = 5;
+//  b = 6;
+// c = a+b;
+// z=(c<11) ? "good":"bad"
+//     console.log(c)
+
+// document.getElementById("as").innerHTML = z+"crore";
+// }
+function resestFrom(){
+    document.getElementById("as").innerHTML=resestFrom();
+    
+}
